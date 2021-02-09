22a456742495736c29e6d18b1dbdf5d535232d69
firebird777 <codesmell777@gmail.com>
2021-02-09 12:36:38 +0530

    improvements in Routebuilder(MessageReceiver.java)

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 import com.example.demo.dao.ArticleDao;
 import com.example.demo.model.Article;
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
