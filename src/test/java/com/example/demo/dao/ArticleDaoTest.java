46540072014c1ee25f294cb5be1f743b22124c3d
firebird777 <codesmell777@gmail.com>
2021-02-16 18:01:16 +0530

    wrting test cases - casuing error in ArticleDaoTest

--- src/test/java/com/example/demo/dao/ArticleDaoTest.java
+++ src/test/java/com/example/demo/dao/ArticleDaoTest.java
@@ -5,45 +5,34 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 class ArticleDaoTest {
-
-
-
+    @Autowired
+    EntityManager entityManager;
 
     @Autowired
-    private ArticleDao articleDao;
-
+    ArticleDao articleDao;
     @Test
     void findAll() {
-        List<Article> articles = articleDao.findAll();
-        System.out.print(articles.size());
-        assertEquals(11,articles.size());
     }
 
     @Test
     void findById() {
-        Article article = articleDao.findById(11);
-        assertEquals(11,article.getArticleId(),"no article found at given id");
-
+        Article article = articleDao.findById(1);
+        assertEquals(1,article.getArticleId());
     }
 
-    //where is the data with id 1 going? if not in database
     @Test
-    @Transactional
     void save() {
-        Article article = new Article(12,"cnn","jcj",10,"njns","cjn",true,false);
-        Article theArticle = articleDao.save(article);
-        assertNotNull(theArticle);
-        assertEquals(article.getArticleId(),theArticle.getArticleId());
+    }
+
+    @Test
+    void deleteById() {
     }
 }
